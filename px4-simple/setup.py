d8eb38487dc2fa8e1865a2c697247fc67633837b
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-04-30 17:53:51 +0200

    Fix entry point path for px4_simple_node in setup.py

--- px4-simple/setup.py
+++ px4-simple/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'px4_simple_node = px4_simple.px4_simple_node:main',
+            'px4_simple_node = px4-simple.px4_simple_node:main',
         ],
     },
 )
