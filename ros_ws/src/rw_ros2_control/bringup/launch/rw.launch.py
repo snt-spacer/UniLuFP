a5fcb515e0da84c68c98bab1f677e7345e477767
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-05-26 14:13:34 +0200

    Rename controller types in rw_controller.yaml for consistency and update default controller type in rw.launch.py

--- ros_ws/src/rw_ros2_control/bringup/launch/rw.launch.py
+++ ros_ws/src/rw_ros2_control/bringup/launch/rw.launch.py
@@ -38,7 +38,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "controller_type",
-            default_value="forward_position_controller",
+            default_value="forward_velocity_controller",
             description="Controller type to use.",
         )
     )
