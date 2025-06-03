07ad31f374eb8aada1a335361391c31e7541e61e
Ricard <ricardmarsalicastan@gmail.com>
2025-06-03 11:36:54 +0200

    correct volumes for docker l4t

--- docker/run_L4T.sh
+++ docker/run_L4T.sh
@@ -2,7 +2,9 @@
 xhost +
 docker run --name unilufp-ros-deploy-container -it --runtime=nvidia -e "ACCEPT_EULA=Y" --rm --network host --ipc host \
     -v $HOME/.Xauthority:/root/.Xauthority \
-    -v ${PWD}/ros_ws/src:/UniLuFP/ros_ws/src/px4_simple \
+    -v ${PWD}/ros_ws/src/px4_simple:/UniLuFP/ros_ws/src/px4_simple \
+    -v ${PWD}/ros_ws/src/pingu_low_level_control:/UniLuFP/ros_ws/src/pingu_low_level_control \
+    -v ${PWD}/ros_ws/src/rw_ros2_control:/UniLuFP/ros_ws/src/rw_ros2_control \
     -v ${PWD}/CAN_Setup:/UniLuFP/CAN_Setup \
     -e DISPLAY \
     -e "PRIVACY_CONSENT=Y" \
