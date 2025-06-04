dd91b9ffa18d76c780c7f3158f2c0af9bbf09d33
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-06-04 11:45:39 +0200

    Remove unused launch files and URDF descriptions from pingu_ros2_control package

--- ros_ws/src/pingu_ros2_control/bringup/launch/pingu.launch.py
+++ ros_ws/src/pingu_ros2_control/bringup/launch/pingu.launch.py
@@ -16,6 +16,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     # Set package name
     package = FindPackageShare("pingu_ros2_control")
+    description_package = FindPackageShare("pingu_description")
     arm_package = FindPackageShare("levion_arm_ros2_control")
     rw_package = FindPackageShare("rw_ros2_control")
 
@@ -55,7 +56,7 @@ def generate_launch_description():
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([package, "urdf", "pingu.urdf.xacro"]),
+            PathJoinSubstitution([description_package, "urdf", "pingu.urdf.xacro"]),
             " ",
             "prefix:=",
             prefix,
@@ -75,7 +76,7 @@ def generate_launch_description():
     )
     rviz_config_file = PathJoinSubstitution(
         [
-            package,
+            description_package,
             "rviz",
             "rw.rviz",
         ]
