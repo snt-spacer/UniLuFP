72ed38c17dd24d7d273785ba70330d6e710ace2c
Ricard <ricardmarsalicastan@gmail.com>
2025-06-04 13:08:11 +0200

    Update run_L4T.sh

--- docker/run_L4T.sh
+++ docker/run_L4T.sh
@@ -5,6 +5,8 @@ docker run --name unilufp-ros-deploy-container -it --runtime=nvidia -e "ACCEPT_E
     -v ${PWD}/ros_ws/src/px4_simple:/UniLuFP/ros_ws/src/px4_simple \
     -v ${PWD}/ros_ws/src/pingu_low_level_control:/UniLuFP/ros_ws/src/pingu_low_level_control \
     -v ${PWD}/ros_ws/src/rw_ros2_control:/UniLuFP/ros_ws/src/rw_ros2_control \
+    -v ${PWD}/ros_ws/src/pingu_cmd_mux:/UniLuFP/ros_ws/src/pingu_cmd_mux \
+    -v ${PWD}/ros_ws/src/zeroG_pingu_launcher:/UniLuFP/ros_ws/src/zeroG_pingu_launcher \
     -v ${PWD}/CAN_Setup:/UniLuFP/CAN_Setup \
     -e DISPLAY \
     -e "PRIVACY_CONSENT=Y" \
