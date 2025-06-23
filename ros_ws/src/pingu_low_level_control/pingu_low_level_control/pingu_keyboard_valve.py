5fc3e225dffc84159bc4e4f76dc10ea90fa5c61a
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-06-23 16:32:49 +0200

    Refactor keyboard input handling in KeyboardPublisher to use terminal settings instead of threading

--- ros_ws/src/pingu_low_level_control/pingu_low_level_control/pingu_keyboard_valve.py
+++ ros_ws/src/pingu_low_level_control/pingu_low_level_control/pingu_keyboard_valve.py
@@ -1,9 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-from pynput import keyboard
-import threading
+import sys, select, termios, tty
 import time
+
+settings = termios.tcgetattr(sys.stdin)
 
 class KeyboardPublisher(Node):
     """
@@ -15,39 +16,32 @@ class KeyboardPublisher(Node):
         self.timer = self.create_timer(1.0 / 50.0, self.timer_callback)  # 50Hz
 
         # Store key states
-        self.keys = ['1','2','3','4','5','6','7','8','9','0']
+        self.keys = ['a','s','1','2','3','4','5','6','7','8']
         self.key_state = {k: 0.0 for k in self.keys}
 
         # Start the keyboard listener in a separate thread
-        listener_thread = threading.Thread(target=self.keyboard_listener, daemon=True)
-        listener_thread.start()
 
-    def timer_callback(self):
+    def timer_callback(self):  
+        key = self.getKey()
+        if key in self.keys:
+            self.key_state[key] = 1.0 if self.key_state[key] == 0.0 else 0.0
+            self.get_logger().info(f'Key {key} toggled to {self.key_state[key]}')
+
+        elif key == '\x03':  # Ctrl+C to exit
+            self.get_logger().info('Exiting keyboard listener.')
+            rclpy.shutdown()
+
         msg = Float32MultiArray()
         msg.data = [self.key_state[k] for k in self.keys]
         self.publisher_.publish(msg)
+            
 
-    def keyboard_listener(self):
-        def on_press(key):
-            try:
-                k = key.char
-                if k in self.key_state:
-                    self.key_state[k] = 1.0
-                    print(f"Key {k} pressed")
-            except AttributeError:
-                pass
-
-        def on_release(key):
-            try:
-                k = key.char
-                if k in self.key_state:
-                    self.key_state[k] = 0.0
-                    print(f"Key {k} released")
-            except AttributeError:
-                pass
-
-        with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-            listener.join()
+    def getKey(self):
+        tty.setraw(sys.stdin.fileno())
+        select.select([sys.stdin], [], [], 0)
+        key = sys.stdin.read(1)
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        return key
 
 def main(args=None):
     rclpy.init(args=args)
