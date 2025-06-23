b54eda369e2ee52d8ff09a69c47e14796623454d
akiyoshi <uchida.akiyoshi.s3@dc.tohoku.ac.jp>
2025-06-23 16:34:48 +0200

    Fix timestamp assignment in TransformStamped to use message header

--- ros_ws/src/pingu_optitrack/pingu_optitrack/pose_to_tf.py
+++ ros_ws/src/pingu_optitrack/pingu_optitrack/pose_to_tf.py
@@ -17,7 +17,7 @@ class PoseToTF(Node):
 
     def callback(self, msg: PoseStamped):
         t = TransformStamped()
-        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.stamp = msg.header.stamp
         t.header.frame_id = 'world'
         t.child_frame_id = 'base_link'
         t.transform.translation.x = msg.pose.position.x
