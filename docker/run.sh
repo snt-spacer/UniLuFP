6fb52890f73bceba5de22cf925e8c1e24abd71ef
ricard-inho <ricard.marsal@uni.lu>
2026-05-25 11:53:12 +0200

    Enhance Docker setup by adding history persistence and updating .gitignore

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+
 xhost +local:docker
 
 # sudo chmod 666 /dev/ttyUSB0
@@ -11,6 +13,9 @@ xhost +local:docker
 # }
 
 # trap cleanup SIGINT SIGTERM
+
+## Persist bash history across container restarts
+mkdir -p "${SCRIPT_DIR}/.history"
 
 docker run --name unilufp-ros-deploy-container -it \
     --privileged \
@@ -26,9 +31,13 @@ docker run --name unilufp-ros-deploy-container -it \
     -v $HOME/RANS_DeployToRobot:/mnt/ros_ws/src/rans_deploy/RANS_DeployToRobot \
     -v /run/udev:/run/udev \
     -v /dev/bus/usb:/dev/bus/usb \
+    -v "${SCRIPT_DIR}/.history:/history:rw" \
     -e DISPLAY=${DISPLAY} \
     -e QT_X11_NO_MITSHM=1 \
     -e XAUTHORITY=/root/.Xauthority \
+    -e HISTFILE="/history/bash_history" \
+    -e HISTCONTROL="ignoredups:erasedups" \
+    -e PROMPT_COMMAND="history -a; history -c; history -r" \
     unilufp-ros-deploy:latest
 
 # cleanup
